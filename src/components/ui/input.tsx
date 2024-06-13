76fb70f822d068304f61bfbccbfbb32611cbfb0e
Haze10425 <jheee042500@gmail.com>
2024-06-13 23:06:54 +0900

    feat: input 컴포넌트 label 추가

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,36 @@
 import * as React from 'react'
 import { cn } from '@/lib/utils'
+import Typography from '../atom/Typography/Typography'
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  id: string
+  label?: string
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, label, id, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          'flex h-[58px] w-full rounded-[50px] border-input bg-secondary px-[26px] py-[20px] text-base ring-offset-background file:border-0 file:bg-transparent file:text-medium file:font-medium placeholder:text-muted-foreground placeholder:font-normal focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
+      <div className={cn('relative')}>
+        <label
+          htmlFor={id}
+          className={cn('inline-block absolute left-[26px] top-5 text-left')}
+        >
+          <Typography weight="600" color="gray-strong" size="14">
+            {label}
+          </Typography>
+        </label>
+        <input
+          id={id}
+          type={type}
+          className={cn(
+            'flex h-[58px] w-full rounded-[50px] indent-24 border-input bg-secondary px-[26px] py-[20px] text-base ring-offset-background file:border-0 file:bg-transparent file:text-medium file:font-medium placeholder:text-muted-foreground placeholder:font-normal focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
     )
   }
 )
